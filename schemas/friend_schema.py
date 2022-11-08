7753fb12bcf2659d317dff8b8be1d5d791eaad11
jfhaines <joefhaines@gmail.com>
2022-11-09 01:34:33 +1000

    Added friend and friend request controllers

--- schemas/friend_schema.py
+++ schemas/friend_schema.py
@@ -1,7 +1,7 @@
 from init import ma
 from marshmallow import fields
 
-class FriendRequestSchema(ma.Schema):
+class FriendSchema(ma.Schema):
     friend1 = fields.Nested('UserSchema', exclude = ['posts', 'comments', 'post_reacts', 'comment_reacts', 'friend_requests_sent', 'friend_requests_received', 'friends1', 'friends2'])
     friend2 = fields.Nested('UserSchema', exclude = ['posts', 'comments', 'post_reacts', 'comment_reacts', 'friend_requests_sent', 'friend_requests_received', 'friends1', 'friends2'])
 
